841de75fd303e02fbd159e5c185020d0d5f9fd9f
dnkdevGoa <dk5732492@gmail.com>
2023-11-14 16:44:23 +0530

    change Css

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  debugger
+
   return (
     <div className="App">
+      <TodoList />
+      <TodoList />
       <TodoList />
     </div>
   );
 }
 
 function TodoList() {
-  debugger
-  return <div>TodoList
-  </div>
+  return (
+    <div>
+      <h3>What to Learn</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <li><input type="checkbox"  checked={true} /><span>CSS&HTML</span></li>
+        <li><input type="checkbox"  checked={true} /><span>JS</span></li>
+        <li><input type="checkbox"  checked={false} /><span>REACT</span></li>
+      </ul>
+    </div>
+  )
 }
 export default App;
